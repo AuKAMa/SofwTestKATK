aadca28cb4f2559f783ca7a7bcacf0ebc75f9404
Kati <kati.auranen@tuni.fi>
2023-11-13 15:18:08 +0200

    Create reduce.test.js

---
+++ tests/reduce.test.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const reduce = require('./reduce');
+
+describe('reduce', () => {
+  it('Reduces an array of numbers to their sum', () => {
+    const result = reduce([1, 2, 3], (sum, n) => sum + n, 0);
+    expect(result).to.equal(6);
+  });
+
+  it('Reduces an object to an accumulated value', () => {
+    const result = reduce({ 'a': 1, 'b': 2, 'c': 1 }, (result, value, key) => {
+      (result[value] || (result[value] = [])).push(key);
+      return result;
+    }, {});
+    expect(result).to.deep.equal({ '1': ['a', 'c'], '2': ['b'] });
+  });
+
+  it('Uses the first element of the array as the initial value if not provided', () => {
+    const result = reduce([1, 2, 3], (product, n) => product * n);
+    expect(result).to.equal(6);
+  });
+
+  // Add more ?...
+});
